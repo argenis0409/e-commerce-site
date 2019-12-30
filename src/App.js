23708174ef870347df4a971401bf76b411901c10
Argenis De Los Santos <argenis04_09@icloud.com>
2019-12-29 23:25:39 -0800

    updated the app component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div>
-     <h1>This is the main app</h1>
+    <div className='container'>
+      <div className='row'>
+        <div className='col-6'>Column</div>
+        <div className='col-6'>Column two</div>
+      </div>
     </div>
   );
 }
